ea5bcfddd4fdfe7fa32e8e1309c989a4a785c8ab
cahfofpai <cahfofpai@magenta.de>
2022-07-08 19:04:14 +0200

    Add storage functionality

--- README.md
+++ README.md
@@ -1,11 +1,46 @@
 # LPAStore
-Short for *Linux Phone Apps Store*
+
+Short for _Linux Phone Apps Store_
 
 ## Development
 
-You can start the backend with `uvicorn main:app --reload`
+You can start the backend with
+
+```
+uvicorn main:app --reload
+```
 
 You can easily query the API endpoints using curl (and prettify it with jq): `curl -s localhost:8000 | jq`
+
+### curl
+
+You can easily query the API endpoints using curl (and prettify it with jq):
+
+```bash
+curl -s localhost:8000 | jq
+```
+
+Include the header with the status code (does not work in combination with jq):
+
+```bash
+curl -i localhost:8000
+```
+
+Make a put request:
+
+```bash
+curl -s -X 'PUT' \
+  'http://localhost:8000/apps/test' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "id": "string",
+  "appdata": "string",
+  "name": "string",
+  "description": "string"
+}' | jq
+```
+
+Hint: With zsh it is much easier to edit multi line commands than with bash
 
 # License
 
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import os
+import json
 
 app = FastAPI()
 
@@ -9,42 +11,73 @@ class App(BaseModel):
     appdata: str | None = None
     name: str | None = None
     description: str | None = None
+    mobile_compatibility: int | None = None
+
+
+@app.on_event("startup")
+def startup():
+    # generated with `figlet -w 50 -c Welcome to LPAStore -f slant`
+    print(
+        """
+     _       __     __                        
+    | |     / /__  / /________  ____ ___  ___ 
+    | | /| / / _ \/ / ___/ __ \/ __ `__ \/ _ \\
+    | |/ |/ /  __/ / /__/ /_/ / / / / / /  __/
+    |__/|__/\___/_/\___/\____/_/ /_/ /_/\___/ 
+                                              
+                      __      
+                     / /_____ 
+                    / __/ __ \\
+                   / /_/ /_/ /
+                   \__/\____/ 
+                              
+      __    ____  ___   _____ __                
+     / /   / __ \/   | / ___// /_____  ________ 
+    / /   / /_/ / /| | \__ \/ __/ __ \/ ___/ _ \\
+   / /___/ ____/ ___ |___/ / /_/ /_/ / /  /  __/
+  /_____/_/   /_/  |_/____/\__/\____/_/   \___/ 
+    """
+    )
+
+    # TODO: Make data dir configurable
+    if not os.path.exists("data"):
+        os.makedirs("data")
+        # TODO: Unified logging
+        print("Data dir created")
+
+    if not os.path.exists("data/apps"):
+        os.makedirs("data/apps")
+        # TODO: Unified logging
+        print("Dir data/apps created")
 
 
 @app.get("/")
 def get_root():
     return {"greeting": "Hello world!"}
-    # return {"Hello world!"}
 
 
 @app.get("/apps")
 def get_app_list():
-    # TODO: Return App
-    return [
-        {
-            "app_id": "test",
-            "name": "Name",
-            "description": "Lorem ipsum dolor sit amet",
-        },
-        {
-            "app_id": "test 2",
-            "name": "Name",
-            "description": "Lorem ipsum dolor sit amet",
-        },
-    ]
+    dir = "data/apps/"
+    apps: list[App] = []
+    for file in os.listdir(dir):
+        apps.append(App.parse_file(f"{dir}{file}"))
+    return apps
 
 
-@app.get("/apps/{app_id}")
-def get_app(app_id: str):
-    # TODO: Return App
-    return {
-        "app_id": app_id,
-        "name": "Name",
-        "description": "Lorem ipsum dolor sit amet",
-    }
+@app.get("/apps/{id}")
+def get_app(id: str):
+    try:
+        return App.parse_file(f"data/apps/{id}.json")
+    except:
+        # TODO: Other status code when file can not be parsed (although this should not happen)
+        raise HTTPException(404)
 
 
-# add information for commit (name, email address)
-@app.put("/apps/{app_id}")
-def put_app(app_id: str, app: App):
+# TODO: Add information for commit (name, email address, message)
+@app.put("/apps/{id}")
+def create_app(id: str, app: App):
+    with open(f"data/apps/{id}.json", "w") as file:
+        file.write(json.dumps(app.dict(), indent=4))
+    # TODO: Does it make sense to return the app here?
     return app
