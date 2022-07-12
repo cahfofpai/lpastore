eabf0c69dfa40e43d638ca72fb76c6061b938b78
cahfofpai <cahfofpai@magenta.de>
2022-07-12 17:00:32 +0200

    Add Svelte Material UI

--- frontend/README.md
+++ frontend/README.md
@@ -17,3 +17,22 @@ Build
 ```bash
 npm run build
 ```
+
+Add package as dev dependency and install it
+
+```bash
+npm install --save-dev <package>
+npm i -D <package>
+```
+
+Remove package (also from package.json)
+
+```bash
+npm r <package>
+```
+
+Update style
+
+```bash
+npm run prepare
+```
--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,64 +1,25 @@
 <script>
-  let count = 0;
-  const increment = () => {
-    count += 1;
-  };
+    import "@smui/common"
+    import Button from "@smui/button"
+
+    let count = 0
+    const increment = () => {
+        count += 1
+    }
 </script>
 
 <main>
-  <h1>Welcome to the LPAStore!</h1>
+    <h3>Welcome to the LPAStore!</h3>
 
-  <h2>It features <b>{count}</b> apps for GNU/Linux on phones</h2>
+    <h4>It features <b>{count}</b> apps for GNU/Linux on phones</h4>
 
-  <button on:click={increment}> Add app </button>
+    <Button on:click={increment}>Add app</Button>
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  button {
-    font-family: inherit;
-    font-size: inherit;
-    padding: 1em 2em;
-    color: #ff3e00;
-    background-color: rgba(255, 62, 0, 0.1);
-    border-radius: 2em;
-    border: 2px solid rgba(255, 62, 0, 0);
-    outline: none;
-    width: 200px;
-    font-variant-numeric: tabular-nums;
-    cursor: pointer;
-  }
-
-  button:focus {
-    border: 2px solid #ff3e00;
-  }
-
-  button:active {
-    background-color: rgba(255, 62, 0, 0.2);
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
+    main {
+        text-align: center;
+        padding: 1em;
+        margin: 0 auto;
     }
-  }
 </style>
