9de9387aaafe7a9e84b55714dd26f058f69073b9
cahfofpai <cahfofpai@magenta.de>
2022-07-13 22:41:28 +0200

    Add routify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
-import autoPreprocess from "svelte-preprocess"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svelte({ preprocess: autoPreprocess() })],
+    plugins: [svelte()],
 })
