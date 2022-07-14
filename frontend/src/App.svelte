4ac1decda3d761505e2624ea522b5489dca147e4
cahfofpai <cahfofpai@magenta.de>
2022-07-14 12:16:58 +0200

    Migrate to Svelte Materialify

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,23 +1,69 @@
 <script>
-    import "@smui/common"
-    import Button from "@smui/button"
-    import Menu from "./lib/Menu.svelte"
+    import {
+        mdiAllInclusive,
+        mdiDotsVertical,
+        mdiMagnify,
+        mdiPlus,
+        mdiPencil,
+    } from "@mdi/js"
+    import {
+        MaterialApp,
+        Button,
+        AppBar,
+        Icon,
+        Menu,
+        ListItem,
+    } from "svelte-materialify"
 
     let count = 0
     const increment = () => {
         count += 1
     }
+
+    // let app_bar_background_color = "orange darken-2"
+    let app_bar_background_color = "deep-orange lighten-1"
+    let app_bar_text_color = "white-text"
 </script>
 
-<Menu />
-
-<main>
-    <h3>Welcome to the LPAStore!</h3>
-
-    <h4>It features <b>{count}</b> apps for GNU/Linux on phones</h4>
-
-    <Button on:click={increment}>Add app</Button>
-</main>
+<MaterialApp>
+    <AppBar class="{app_bar_background_color} {app_bar_text_color}">
+        <span slot="title">
+            <Icon path={mdiAllInclusive} class={app_bar_text_color} />
+            LPAStore
+        </span>
+        <div style="flex-grow: 1" />
+        <Button fab depressed class={app_bar_background_color}>
+            <Icon path={mdiMagnify} />
+        </Button>
+        <Menu right>
+            <div slot="activator">
+                <Button fab depressed class={app_bar_background_color}>
+                    <Icon path={mdiDotsVertical} />
+                </Button>
+            </div>
+            <ListItem>
+                <span slot="prepend">
+                    <Icon path={mdiPlus} />
+                </span>
+                Add app</ListItem
+            >
+            <ListItem>
+                <span slot="prepend">
+                    <Icon path={mdiPencil} />
+                </span>
+                Edit app
+            </ListItem>
+        </Menu>
+    </AppBar>
+    <Menu />
+    <main>
+        <h3>Welcome to the LPAStore!</h3>
+        <br />
+        <h4>It features <b>{count}</b> apps for GNU/Linux on phones</h4>
+        <br />
+        <Button on:click={increment}>Add app</Button>
+    </main>
+</MaterialApp>
 
 <style>
     main {
